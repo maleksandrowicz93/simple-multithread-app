d11c8293dcaafc30d9e585d5d9abceb93f0e79ab
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2020-03-03 21:42:32 +0100

    add multithread app

---
+++ src/main/java/app/OtodomMultithread.java
@@ -0,0 +1,39 @@
+package app;
+
+import java.io.*;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class OtodomMultithread {
+
+    //2473
+
+    public static void main(String[] args) throws IOException {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(30);
+
+        long start = System.currentTimeMillis();
+
+        String webSiteUrl = "https://www.otodom.pl/sprzedaz/mieszkanie/sopot/";
+        String content = Helper.getWebSite(webSiteUrl);
+        Set<String> setOfLinks = Helper.getSetOfLinks(content);
+
+        for (int i = 0; i < setOfLinks.size(); i++) {
+            int finalI = i;
+            executorService.submit(() -> {
+                try {
+                    Helper.readWebsite(setOfLinks.toArray()[finalI].toString(), finalI + ".html");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        executorService.shutdown();
+
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+
+    }
+
+}
