e36e2895bc7f0456d5e5e82d375a29eb52e349d3
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2020-03-03 21:37:28 +0100

    add single thread app

---
+++ src/main/java/app/OtodomSingleThread.java
@@ -0,0 +1,27 @@
+package app;
+
+import java.io.*;
+import java.util.Set;
+
+public class OtodomSingleThread {
+
+    //63770
+
+    public static void main(String[] args) throws IOException {
+
+        long start = System.currentTimeMillis();
+
+        String webSiteUrl = "https://www.otodom.pl/sprzedaz/mieszkanie/sopot/";
+        String content = Helper.getWebSite(webSiteUrl);
+        Set<String> setOfLinks = Helper.getSetOfLinks(content);
+
+        for (int i = 0; i < setOfLinks.size(); i++) {
+            Helper.readWebsite(setOfLinks.toArray()[i].toString(), i + ".html");
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+
+    }
+
+}
